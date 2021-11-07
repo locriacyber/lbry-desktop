1a84a7e11e2c2f234a699d7964ce62f210f36b01
Locria Cyber <locriacyber@noreply.users.github.com>
2021-11-07 08:26:58 +0800

    Upgrade babel and scss

--- babel.config.js
+++ babel.config.js
@@ -8,9 +8,10 @@ module.exports = api => {
       '@babel/plugin-transform-runtime',
       ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
       '@babel/plugin-transform-flow-strip-types',
-      '@babel/plugin-proposal-class-properties',
+      ['@babel/plugin-proposal-class-properties', {loose: true}],
       'react-hot-loader/babel',
     ],
     ignore: [/node_modules/],
   };
 };
+``
